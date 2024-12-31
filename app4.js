099713c453b401b39cd4369de459fba1650a515e
Arnab Pachal <ap.23cs8031@nitdgp.ac.in>
2024-12-31 19:40:18 +0530

    commited

--- app4.js
+++ app4.js
@@ -4,7 +4,6 @@ let shayeri=document.querySelectorAll(".shayeri");
 let divs= document.querySelectorAll(".songs");
 let strbtn1=document.querySelector(".start1");
 let strbtn2=document.querySelector(".start2");
-let strbtn3=document.querySelector(".start3");
 
 let res=0;
 let h=0;
@@ -28,36 +27,20 @@ divs[0].addEventListener("click",()=>{
 
 })
 
+
 divs[1].addEventListener("click",()=>{
- if(h==0){shayeri[1].play();
-   alert(" ITS WISH OF ARNAB TO AYANTIKA\n CLICK AGAIN TO PAUSE");
+ if(i==0){shayeri[1].play();
+   alert(" SECOND SHAYERI FOR AYANTIKA\n CLICK AGAIN TO PAUSE");
    strbtn2.setAttribute("src","on.jpg");
    divs[1].style.border="3px solid black"
    divs[1].style.backgroundColor="#fb25a5";
    
- h=1;}
+ i=1;}
  else{shayeri[1].pause();
    strbtn2.setAttribute("src","off.jpg");
-   alert("ITS PAUSED");
-   divs[1].style.border="2px solid black"
-   divs[1].style.backgroundColor="#f582c7";
- h=0;}
-
-})
-
-divs[2].addEventListener("click",()=>{
- if(i==0){shayeri[2].play();
-   alert(" SECOND SHAYERI FOR AYANTIKA\n CLICK AGAIN TO PAUSE");
-   strbtn3.setAttribute("src","on.jpg");
-   divs[2].style.border="3px solid black"
-   divs[2].style.backgroundColor="#fb25a5";
-   
- i=1;}
- else{shayeri[2].pause();
-   strbtn3.setAttribute("src","off.jpg");
    alert("ITS PAUSED");i=0;
-   divs[2].style.border="2px solid black"
-   divs[2].style.backgroundColor="#f582c7";}
+   divs[1].style.border="2px solid black"
+   divs[1].style.backgroundColor="#f582c7";}
 
 })
 let img1 =document.querySelector(".img1");
